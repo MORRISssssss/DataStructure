a01014a7a7f769db7ffaba1b6d3940e2ce3fe866
MorisZheng <s04044mort@gmail.com>
2023-03-30 00:19:28 +0800

    update

--- ADT2.3 Polynomial/Polynomial.cpp
+++ ADT2.3 Polynomial/Polynomial.cpp
@@ -6,17 +6,47 @@ Polynomial::Polynomial()
 {
     capacity = 5;
     terms = 0;
-    NewTerm(0, 0);
+    termArray = new Term[capacity];
+    termArray[0].coef = 0;
+    termArray[0].exp = 0;
 }
 
-Polynomial Polynomial::Add(const Polynomial poly)
+Polynomial Polynomial::Add(const Polynomial b)
 {
-    return Polynomial();
+    Polynomial c; //c.terms = 0
+    int aPos = 0, bPos = 0;
+    while ((aPos < terms) && (bPos < b.terms)){
+            if (termArray[aPos].exp == b.termArray[bPos].exp) {
+            float t = termArray[aPos].coef + b.termArray[bPos].coef;
+            if (t) c.NewTerm(t, termArray[aPos].exp);
+            aPos++; bPos++;
+        }
+        else if (termArray[aPos].exp < b.termArray[bPos].exp) {
+            c.NewTerm(b.termArray[bPos].coef, b.termArray[bPos].exp);
+            bPos++;
+        }
+        else {
+            c.NewTerm(termArray[aPos].coef, termArray[aPos].exp);
+            aPos++;
+        }
+    } 
+    //add in remaining terms of *this
+    for ( ; aPos < terms ; aPos++)//output the remaining terms
+        c.NewTerm (termArray[aPos].coef, termArray[aPos].exp);
+    //add in remaining terms of b
+    for ( ; bPos < b.terms ; bPos++)//output remaining terms
+        c.NewTerm (b.termArray[bPos].coef, b.termArray[bPos].exp);
+    return c;
 }
 
 Polynomial Polynomial::Mult(const Polynomial poly)
 {
     return Polynomial();
+}
+
+float Polynomial::Eval(const float f)
+{
+    return 0.0f;
 }
 
 int Polynomial::operator!()
@@ -43,6 +73,15 @@ void Polynomial::NewTerm(const float theCoeff, const int theExp)
         delete [] termArray ; // delete allocated old memory
         termArray = temp;
     }
-    termArray [terms].coef = theCoeff;
-    termArray [terms++].exp = theExp;
+    termArray[terms].coef = theCoeff;
+    termArray[terms++].exp = theExp;
+}
+
+ostream& operator<<(ostream& os, const Polynomial& poly)
+{
+
+}
+
+istream& operator>>(istream& os, Polynomial& poly){
+
 }
--- ADT2.3 Polynomial/Polynomial.h
+++ ADT2.3 Polynomial/Polynomial.h
@@ -16,23 +16,23 @@ class Term {
 class Polynomial {
     public:
         //construct the polynomial p(x) = 0
-        Polynomial( );
+        Polynomial();
         //return the sum of *this and poly
-        Polynomial Add(const Polynomial poly);
+        Polynomial Add(const Polynomial);
         //return the product of *this and poly
-        Polynomial Mult(const Polynomial poly);
+        Polynomial Mult(const Polynomial);
         //Evaluate the polynomial *this at f and return the results
-        float Eval(const float f );
+        float Eval(const float);
         // if *this is the zero polynomial, return 1; else return 0;
         int operator!();
         // return the coefficient of e in *this
-        float Coef(const int e);
+        float Coef(const int);
         // return the largest exponent in *this
         int LeadExp();
         // Add a new term to the end of termArray
-        void NewTerm(const float theCoeff, const int theExp);
-        friend ostream& operator<<(ostream& os, const Polynomial poly);
-        friend istream& operator>>(istream& os, Polynomial poly);
+        void NewTerm(const float, const int);
+        friend ostream& operator<<(ostream&, const Polynomial&);
+        friend istream& operator>>(istream&, Polynomial&);
     private:
         Term *termArray;
         int capacity;
