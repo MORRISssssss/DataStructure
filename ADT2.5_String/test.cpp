2681ce35ce6a573c0e87472f44a1e19dcde4bb9d
MorisZheng <s04044mort@gmail.com>
2023-04-02 01:45:55 +0800

    update 4/2-4

---
+++ ADT2.5_String/test.cpp
@@ -0,0 +1,16 @@
+#include "String.h"
+#include "String.cpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    String A, B, C;
+    cout << "Enter String A: ";
+    cin >> A;
+    cout << "String A: " << A << endl;
+    cout << "Enter String B: ";
+    cin >> B;
+    cout << "String B: " << B << endl;
+    
+}
